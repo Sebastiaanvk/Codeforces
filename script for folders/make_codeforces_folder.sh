6df71c7fca7b10e6e7decff7ad6c0ee90496e028
Sebastiaanvk <sebastiaanvk96@gmail.com>
2022-03-10 00:47:55 +0100

    Shell script for codeforces files

---
+++ script for folders/make_codeforces_folder.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+letters="ABCDEFGHIJ"
+echo What contest are you in?
+
+read contest
+
+echo $contest? That\'s awesome!
+
+echo How many question are there?
+
+read questions
+
+mkdir ../$contest
+
+for ((i=0; i< $questions; i++)) 
+do
+	dest="../$contest/Problem\ "${letters:$i:1}""
+	letter="${letters:$i:1}"
+	mkdir ../$contest/Problem\ "${letters:$i:1}"
+	
+	cp ./standard/main.cpp ../$contest/Problem\ "${letters:$i:1}"/main.cpp 
+
+	cp ./standard/standard.cbp ../$contest/Problem\ "${letters:$i:1}"/Problem\ $letter.cbp
+
+	cp ./standard/standard.layout ../$contest/Problem\ "${letters:$i:1}"/Problem\ $letter.layout
+	string="Problem "$letter
+	sed -i -e "s/standard/$string/g" ../$contest/Problem\ "${letters:$i:1}"/Problem\ $letter.cbp 
+done
+
