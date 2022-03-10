b6c9e0add50a9d3f157dbe0e351aeeaa930fd04b
Sebastiaanvk <sebastiaanvk96@gmail.com>
2022-03-10 01:00:53 +0100

    Fixed the folder script

--- script for folders/make_codeforces_folder.sh
+++ script for folders/make_codeforces_folder.sh
@@ -4,26 +4,25 @@ echo What contest are you in?
 
 read contest
 
-echo $contest? That\'s awesome!
+echo "$contest"? That\'s awesome!
 
 echo How many question are there?
 
 read questions
 
-mkdir ../$contest
+mkdir ../"$contest"
 
 for ((i=0; i< $questions; i++)) 
 do
-	dest="../$contest/Problem\ "${letters:$i:1}""
 	letter="${letters:$i:1}"
-	mkdir ../$contest/Problem\ "${letters:$i:1}"
+	mkdir ../"$contest"/Problem\ "${letters:$i:1}"
 	
-	cp ./standard/main.cpp ../$contest/Problem\ "${letters:$i:1}"/main.cpp 
+	cp ./standard/main.cpp ../"$contest"/Problem\ "${letters:$i:1}"/main.cpp 
 
-	cp ./standard/standard.cbp ../$contest/Problem\ "${letters:$i:1}"/Problem\ $letter.cbp
+	cp ./standard/standard.cbp ../"$contest"/Problem\ "${letters:$i:1}"/Problem\ $letter.cbp
 
-	cp ./standard/standard.layout ../$contest/Problem\ "${letters:$i:1}"/Problem\ $letter.layout
+	cp ./standard/standard.layout ../"$contest"/Problem\ "${letters:$i:1}"/Problem\ $letter.layout
 	string="Problem "$letter
-	sed -i -e "s/standard/$string/g" ../$contest/Problem\ "${letters:$i:1}"/Problem\ $letter.cbp 
+	sed -i -e "s/standard/$string/g" ../"$contest"/Problem\ "${letters:$i:1}"/Problem\ $letter.cbp 
 done
 
